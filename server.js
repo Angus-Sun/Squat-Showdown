5610fb60dbbcfc0f3da8580c6392227908dbbe1c
TerraYosh <135078117+TerraYosh@users.noreply.github.com>
2024-08-08 13:18:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5147;
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
